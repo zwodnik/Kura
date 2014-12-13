76c1e4b2ca3c5cbfec9c7c28e6cab5be4944ba89
zwodnik <kamil.jarosinski@gmail.com>
2014-12-13 05:56:42 +0100

    okw

--- Kura/Kura.Windows/MainPage.xaml.cs
+++ Kura/Kura.Windows/MainPage.xaml.cs
@@ -13,7 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-using XamlAnimatedGif;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
